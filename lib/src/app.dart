3b32f61bec06e5ce32a76a9ecd461f5c59c0399d
my0866805011 <my0866805011@gmail.com>
2023-03-07 00:00:54 +0700

    ep4.1

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,7 +1,8 @@
-
+import 'package:ais2023/src/bloc/login/login_bloc.dart';
 import 'package:ais2023/src/pages/login/login_page.dart';
 import 'package:ais2023/src/pages/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,13 +10,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+    // ignore: unused_local_variable
+    final loginBloc = BlocProvider(create: (context) => LoginBloc());
+
+
+    return MultiBlocProvider(
+      providers:[],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
           primarySwatch: Colors.blue,
+        ),
+        routes: AppRoute.all,
+        home: const LoginPage(),
       ),
-      routes: AppRoute.all,
-      home: const LoginPage(),
     );
   }
 }
--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -5,96 +5,113 @@ class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
-
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   final TextEditingController _userNameCtl = TextEditingController();
   final TextEditingController _passwordCtl = TextEditingController();
+  int count = 0;
 
   @override
   void initState() {
-      super.initState();
-    _userNameCtl.text ='Admin';
-    _passwordCtl.text ='1234';
+    super.initState();
+    _userNameCtl.text = 'Admin';
+    _passwordCtl.text = '1234';
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login'),
+      appBar: AppBar(
+        title: const Text('Login'),
       ),
       body: Container(
         width: double.infinity,
-        color: Color.fromARGB(255, 9, 116, 179).withOpacity(0.2),
+        color: const Color.fromARGB(255, 9, 116, 179).withOpacity(0.2),
         child: Padding(
           padding: const EdgeInsets.all(32.0),
           child: Card(
-            child:Container(
+            child: Container(
               padding: const EdgeInsets.all(32),
-              height: 400,           
-            child: Column(
+              height: 400,
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 //mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   ..._buildTextFields(),
-                  SizedBox(height: 32,),
+                  const SizedBox(
+                    height: 32,
+                  ),
                   ..._buildButtons(),
+                  Row(
+                    children: [
+                      Text("Debug: $count"),
+                      IconButton(
+                        onPressed: () {
+                          count = count + 1;
+                          setState(() {});
+                        },
+                        icon: Icon(Icons.add),
+                      ),
+                      IconButton(onPressed: () {
+                        count = count-1;
+                        setState(() {
+                          
+                        });
+                      },
+                       icon: Icon(Icons.remove),
+                      )
+                    ],
+                  ),
                 ],
-              ),   
+              ),
             ),
           ),
         ),
       ),
-    );  
-  }
-  
- void _userNameReset() {
-    _userNameCtl.text="Admin";
-    _passwordCtl.text='1234';
-  }
-  
- void _printUsername() {
- print("Login with  ${_userNameCtl.text},${_passwordCtl.text}");
- Navigator.pushNamed(context, AppRoute.home);  
+    );
   }
 
-void _buttonResgister() {
-  Navigator.pushNamed(context, AppRoute.register);
-}
+  void _userNameReset() {
+    _userNameCtl.text = "Admin";
+    _passwordCtl.text = '1234';
+  }
 
-  _buildTextFields(){
+  void _printUsername() {
+    print("Login with  ${_userNameCtl.text},${_passwordCtl.text}");
+    Navigator.pushNamed(context, AppRoute.home);
+  }
+
+  void _buttonResgister() {
+    Navigator.pushNamed(context, AppRoute.register);
+  }
+
+  _buildTextFields() {
     return [
       TextField(
-      controller: _userNameCtl,
-      decoration: InputDecoration(labelText: 'Username'),
+        controller: _userNameCtl,
+        decoration: InputDecoration(labelText: 'Username'),
       ),
       TextField(
-      controller: _passwordCtl,
-      decoration: InputDecoration(labelText: 'Password'),
+        controller: _passwordCtl,
+        decoration: InputDecoration(labelText: 'Password'),
       ),
     ];
   }
 
   _buildButtons() {
-    return[
-        ElevatedButton(
-          onPressed: ()=>_printUsername(),
-           child: const Text('Login')
-            ),
-         
-         OutlinedButton(onPressed: ()=>_buttonResgister(), 
-           child: const Text('Register'),
-          ),
-
-        OutlinedButton(onPressed: ()=>_userNameReset(), 
-           child: const Text('Reset'),
-          ),
-
+    return [
+      ElevatedButton(
+          onPressed: () => _printUsername(), child: const Text('Login')),
+      OutlinedButton(
+        onPressed: () => _buttonResgister(),
+        child: const Text('Register'),
+      ),
+      OutlinedButton(
+        onPressed: () => _userNameReset(),
+        child: const Text('Reset'),
+      ),
     ];
-  
-
   }
 }
